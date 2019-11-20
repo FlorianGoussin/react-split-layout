b27f0e37023986f721a24038b1ddef9f6907d40b
Florian Goussin <floriang@eyesopen.com>
2019-11-19 20:52:40 -0700

    fix many issues. size starts to work

--- src/LayoutContainer.js
+++ src/LayoutContainer.js
@@ -9,47 +9,48 @@ const getId = () => (Date.now() * Math.random()).toString(36).replace('.', '')
 
 export const LayoutItem = (props) => {
   const ref = useRef(null)
-  // props.setGetSizeCallback()
   const getSize = () => ref.current ? ref.current.offsetWidth : 0
+  const style = { [props.propName]: props.size }
+  props.getLayoutItemSize(getSize)
   return (
-    <div ref={ref} className="item">{props.children}</div>
+    <div ref={ref} style={style} className="item">{props.children}</div>
   )
 }
 
-const RESIZER_VALUE = 4 // pixels
-
 export const LayoutContainer = (props) => {
+  const RESIZER_VALUE = 4 // pixels
   const ref = useRef(null)
   const isRow = props.direction === 'row'
   const propName = isRow ? 'width' : 'height'
+  const queryLayoutItemSize = [] // contains all the getSize functions from child
   const [sizes, setSizes] = useState([])
 
-  let containerSize = 0
-
   // remove respectively the resizers with or height from the layout container width or height
-  const getResizersSize = () => (props.children.length - 1) * RESIZER_VALUE
-  const getContainerSize = () => ref.current ? ref.current.offsetWidth - getResizersSize() : 0
+  const getResizersSize = layoutItems => (layoutItems.length - 1) * RESIZER_VALUE
+  const getContainerSize = resiserSize => ref.current ? (ref.current.offsetWidth - resiserSize) : 0
 
-  const getItemUserDefinedSizes = (containerSize, layoutItems) => {
+  useEffect(() => {
+    const containerSize = getContainerSize(getResizersSize(props.children))
     const sizesPercent =
-      layoutItems.map(layoutItem => parseFloat(layoutItem.props[propName] || 0))
+      props.children.map(layoutItem => parseFloat(layoutItem.props[propName] || 0))
+
     const invalid = sizesPercent.some(size => size > (100 - sizesPercent.length)) ||
                     sizesPercent.reduce((a, b) => a + b) > 100
     if (invalid) throw new Error('LayoutItem size should be less than 100%')
-    return sizesPercent
-      .map(userSize => userSize * containerSize / 100)
-  }
 
-  useEffect(() => {
-    const containerSize = getContainerSize()
-    const itemInitialSizes = getItemUserDefinedSizes(containerSize, props.children)
+    const itemInitialSizes = sizesPercent.map(userSize => userSize * containerSize / 100)
     setSizes(itemInitialSizes)
-  }, [])
+
+  }, [props.children, propName])
 
   // TODO: move all of these to Resizer component
   const onMouseDownOnResizer = () => {
     document.removeEventListener('mousemove', onMousemove)
     document.removeEventListener('mouseup', onMouseup)
+
+    const containerSize = getContainerSize(getResizersSize(props.children))
+    const oldContainerSize = sizes.reduce((a, b) => a + b)
+    setSizes(sizes.map(size => size * containerSize / oldContainerSize))
   }
 
   const onMousemove = () => {
@@ -61,7 +62,7 @@ export const LayoutContainer = (props) => {
     document.removeEventListener('mouseup', onMouseup)
   }
 
-  const getResizer = () => {
+  const getResizer = (itemIdx) => {
     return (
       <div className={`resizer ${props.direction}`}
            onMouseDown={onMouseDownOnResizer}
@@ -71,13 +72,17 @@ export const LayoutContainer = (props) => {
   }
 
   const getItems = () => {
-    const layoutItemProps = {}
     const layoutItems = props.children.map((layoutItem, idx) => {
-      layoutItemProps.key = getId()
-      return React.cloneElement(layoutItem, layoutItemProps)
+      console.log(sizes)
+      return React.cloneElement(layoutItem, {
+        key: getId(),
+        size: sizes[idx],
+        propName,
+        getLayoutItemSize: getSize => { queryLayoutItemSize[idx] = getSize }
+      })
     })
-    return layoutItems.slice(1).reduce((arr, child) =>
-      [...arr, getResizer(), child], [layoutItems[0]]
+    return layoutItems.slice(1).reduce((arr, layoutItem, idx) =>
+      [...arr, getResizer(idx + 1), layoutItem], [layoutItems[0]]
     ) || []
   }
 
