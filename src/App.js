64e12d49c3e5eb35aa3af0b0693e448fc5d17977
Florian Goussin <floriang@eyesopen.com>
2019-11-20 19:38:49 -0700

    add a more complex layout to test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ const App = () => {
   return (
     <LayoutContainer root={true} direction={'row'}>
       <LayoutItem>c1</LayoutItem>
-      <LayoutItem width="50">c2</LayoutItem>
+      <LayoutItem width="70">
+        <LayoutContainer root={true} direction={'column'}>
+          <LayoutItem>c2 r1</LayoutItem>
+          <LayoutItem height="60">
+            <LayoutContainer root={true} direction={'row'}>
+              <LayoutItem>c2 r1 c1</LayoutItem>
+              <LayoutItem>c2 r1 c2</LayoutItem>
+              <LayoutItem>c2 r1 c3</LayoutItem>
+            </LayoutContainer>
+          </LayoutItem>
+          <LayoutItem>c2 r3</LayoutItem>
+        </LayoutContainer>
+      </LayoutItem>
       <LayoutItem>c3</LayoutItem>
     </LayoutContainer>
   )
