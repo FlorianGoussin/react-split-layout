3570736ef4d416ce7a0945d5c81a1c071412cf2c
Florian Goussin <floriang@eyesopen.com>
2019-11-25 17:34:48 -0700

    use redux to manage state

--- src/components/LayoutRoot.js
+++ src/components/LayoutRoot.js
@@ -1,10 +1,60 @@
 import './LayoutRoot.css'
-import React from 'react';
+import React, { useEffect } from 'react';
+// import buildStateFromTree
 
 export const LayoutRoot = props => {
+  // useEffect(() => {
+  //   props.state ||
+  // })
   return (
     <div className={`layout-root`}>
       {props.children}
     </div>
   )
 }
+
+// Reuse of Golden Layout State architechture using Redux
+// https://golden-layout.com/
+// Object to be serialized:
+// state = {
+//   "content": [
+//     "type": "row",
+//     "content": [
+//       {
+//         "type": "component"
+//       },
+//       {
+//         "type": "column",
+//         "width": 70,
+//         "content": [
+//           {
+//             "type": "component"
+//           },
+//           {
+//             "type": "component"
+//           },
+//         ]
+//       },
+//       {
+//         "type": "component"
+//       }
+//     ],
+//   ],
+// }
+//
+// Redux state:
+// state = {
+//   items: [{
+//     parent: parentId
+//     children: [childrenIds]
+//   }],
+//   containers: {
+//     row: []
+//     column: []
+//   },
+//   stacks: []
+// }
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import App from './components/App';
+const store = createStore(App)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
